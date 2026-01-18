4c1b7cfb8ddb4b03407b49114cb1d6c4487e85c3
neo9480 <skyline.ds247@gmail.com>
2026-01-18 19:00:31 +0530

    make it mergable

--- src/queries/db_schema.sql
+++ src/queries/db_schema.sql
@@ -1,3 +1,19 @@
+-- Categorires and Competition Schema
+CREATE TABLE IF NOT EXISTS categories (
+    category_id TEXT PRIMARY KEY,
+    category_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS competitions (
+    competition_id TEXT PRIMARY KEY,
+    competition_name TEXT NOT NULL,
+    parent_id TEXT,
+    type TEXT,
+    gender TEXT,
+    category_id TEXT,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
 -- Competitor and Competitor Rankings Schema
 CREATE TABLE IF NOT EXISTS competitors (
     competitor_id VARCHAR(50) PRIMARY KEY,
